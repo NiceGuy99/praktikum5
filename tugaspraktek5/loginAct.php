4c939b6646cf8a3c7edc1e9544bba0bb05e44e5d
Muhammad Yusuf Ardiansyah <61173562+NiceGuy99@users.noreply.github.com>
2020-03-10 08:41:26 +0700

    Praktikum 5
    tugas praktikum 5

---
+++ tugaspraktek5/loginAct.php
@@ -0,0 +1,14 @@
+<h2>Biodata akun :</h2>
+<?php
+echo " Nama : Muhammad Yusuf Ardiansyah <br>";
+echo " Email : yusuf.ardiansyah2807@gmail.com<br>";
+echo "Alamat rumah: Balongbendo Rt.07 Rw.01 Kab. Sidoarjo<br>";
+echo "Alamat sekarang : Perumahan Ikip blok A/25<br>";
+echo "Tempat, Tanggal lahir : Sidoarjo, 28 July 2000 <br>";
+echo "Hobi : Fotografi dan Mendaki Gunung<br> ";
+echo "Social media : <br> - Instagram : @myusuf1915 <br>- Twitter : @myusuf1915 <br>- Whatsapp : 082139820863 <br>- Line : yusufff_a<br>";
+date_default_timezone_set('Asia/Jakarta');
+echo "<left> Tanggal Sekarang : ";
+echo date("l-d-M-Y, g:i:s a");
+
+?>
